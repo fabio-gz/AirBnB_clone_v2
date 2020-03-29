d0b814556f29467903e41436a3b4f22e4afbd6bc
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-29 20:17:12 +0000

    [Add]use of env var

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,19 +13,18 @@ class DBStorage:
     def __init__(self):
         """The init return the instance"""
 
-        os.environ['HBNB_MYSQL_USER'] = 'hbnb_dev'
-        os.environ['HBNB_MYSQL_PWD'] = 'hbnb_dev_pwd'
-        os.environ['HBNB_MYSQL_HOST'] = 'locahost'
-        os.environ['HBNB_MYSQL_DB'] = 'hbnb_dev_db'
-        os.environ['HBNB_ENV'] = 'dev'
-
         user = os.getenv('HBNB_MYSQL_USER')
         pwd = os.getenv('HBNB_MYSQL_PWD')
         LH = os.getenv('HBNB_MYSQL_HOST')
         DB = os.getenv('HBNB_MYSQL_DB')
 
-        self.__engine = create_engine('mysql+mysqldb://user:pwd@LH:3306/DB',
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.
+                                      format(user, pwd, LH, DB),
                                       pool_pre_ping=True)
 
         if os.getenv('HBNB_ENV') is "test":
-            
+            Base.metada.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """query on the database session and output a dict"""
+        
