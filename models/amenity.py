c9a73424f1d591ea1a946f01d836594292f712fb
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-01 11:04:20 -0500

    [Fix] Delete de if statement

--- models/amenity.py
+++ models/amenity.py
@@ -14,6 +14,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary=place_amenity)
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity)
