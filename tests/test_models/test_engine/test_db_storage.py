0b7facd50bb2436438842c2258ef0716aa8d5e5b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-01 11:11:54 -0500

    [Add] put pass

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""test for file storage"""
+import unittest
+import pep8
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestDBStorage(unittest.TestCase):
+    """This will test the DBStorage"""
+    pass
