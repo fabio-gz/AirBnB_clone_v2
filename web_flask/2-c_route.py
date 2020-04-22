fb1c367a004dbe7b216a836c43ba695eed7d42be
fabio-gz <fabiogomez.silva@gmail.com>
2020-04-22 11:50:30 +0000

    [fix]uppecase

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,20 +3,23 @@
 from flask import Flask, escape
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """prints  hello"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """prints HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def variables(text):
     """print variable"""
-    return 'c {}'.format(escape(text).replace('_', ' '))
+    return 'C {}'.format(escape(text).replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
