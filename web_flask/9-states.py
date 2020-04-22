e063efd9d4d3daa1cbad0a350402996e7535af90
fabio-gz <fabiogomez.silva@gmail.com>
2020-04-22 22:45:49 +0000

    [Add]states and state

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""list of states"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(error):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+@app.route('/states', strict_slashes=False)
+def states_alone():
+    """states"""
+    states = storage.all('State')
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """states id"""
+    states = storage.all('State')
+    return render_template('9-states.html', states=states, id=id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
